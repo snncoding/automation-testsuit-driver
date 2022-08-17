058880ea08528c7629650b03da393c1b0eaecbc5
Dobur <sinan.dobur@capgemini.com>
2022-08-17 13:28:49 +0200

    first commit

---
+++ src/test/java/com/epexspot/automationtestsuitdriver/AutomationTestsuitDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epexspot.automationtestsuitdriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationTestsuitDriverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
